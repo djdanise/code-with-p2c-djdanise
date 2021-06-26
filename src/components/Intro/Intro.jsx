1af32e0860af3c5d395945437d7671d1fff0eb52
Ethan Warnar <enpwarna@uwaterloo.ca>
2021-06-26 07:39:29 -0400

    Cleaning up code

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,55 +1,16 @@
 import React from 'react';
-import { Parallax } from 'react-scroll-parallax';
 import './Intro.scss';
 import { Button, ButtonGroup } from 'reactstrap';
-
-
-
-// const copy = ''.split('');
-// const date = '26-June-2021 | 2-4pm'.split('');
-const topic = 'With  P2C'.split('')
-
 
 const Intro = () => (
     <React.Fragment>
         <div className="buttons">
-            {/* <ButtonGroup>
-                <Button>Contact</Button>
-                <Button>LinkedIn</Button>
-            </ButtonGroup> */}
+            <ButtonGroup>
+                <Button target="_blank" href="https://www.facebook.com/events/197719935560769?ref=newsfeed">P2C</Button>
+                <Button target="_blank" href="https://github.com/ethanwarnar/code-with-p2c-demo">Source Code</Button>
+            </ButtonGroup>
         </div>
-
-        <div className="main">
-            {/* <div className="bar" /> */}
-            {/* <span className="title1">
-                {copy.map((letter, i) => (
-                    <Parallax
-                        key={`copy-${i}`}
-                        className="letter h1"
-                        x={[0 + 'px', ((i - 3.5) * 40) + 'px']}
-                    >
-                        {letter}
-                    </Parallax>
-                ))}
-            </span> */}
-            {/* <span className="title2">
-                {topic.map((letter, i) => (
-                    <Parallax
-                        key={`copy-${i}`}
-                        className="letter h2"
-                        x={[0 + 'px', ((i - 4) * 40) + 'px']}
-                    >
-                        {letter}
-                    </Parallax>
-                ))}
-            </span> */}
-
-
-
-
-
-            {/* <div className="bar" /> */}
-        </div>
+        <div className="main"/>
     </React.Fragment>
 );
 
